ec96c293763dc91f919a28982de35264d49cfa42
Maciej Packiewicz <maciej.packiewicz@proton.me>
2025-03-03 15:50:13 +0100

    Create first version of views

---
+++ BlazorUI/Components/Pages/AlbumLibrary.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BlazorUI.Components.Pages;
+
+public class AlbumLibrary : PageModel
+{
+    public void OnGet()
+    {
+        
+    }
+}
---
+++ BlazorUI/Components/Pages/AlbumSearch.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BlazorUI.Components.Pages;
+
+public class AlbumSearch : PageModel
+{
+    public void OnGet()
+    {
+        
+    }
+}
